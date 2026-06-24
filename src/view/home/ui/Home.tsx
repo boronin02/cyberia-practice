742c97c14b249692185bcddd9b760e3c21e07010
Иван <boronin02@MacBook-Air-Ivan.local>
2026-06-23 19:49:48 -0700

    chore: initial project setup with FSD architecture

---
+++ src/view/home/ui/Home.tsx
@@ -0,0 +1,10 @@
+import { Header } from "@/widgets/Header/ui/Header";
+import { Hero } from "@/widgets/Hero/ui/Hero";
+
+export const Home = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
